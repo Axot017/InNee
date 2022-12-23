8b84b00f74abd20867d40793ce02aa05fdb775f3
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-12-23 20:22:19 +0100

    build(infra): add cognito resources

---
+++ infra/module/auth-module/main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "aws_cognito_user_pool" "pool" {
+  name = "${var.app_name}-${var.env}-pool"
+
+  alias_attributes         = ["email"]
+  auto_verified_attributes = ["email"]
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
+  username_configuration {
+    case_sensitive = false
+  }
+
+  password_policy {
+    minimum_length = 12
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 7
+      max_length = 256
+    }
+  }
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name                                 = "${var.app_name}-${var.env}-client"
+  user_pool_id                         = aws_cognito_user_pool.pool.id
+  generate_secret                      = true
+  allowed_oauth_flows_user_pool_client = true
+  prevent_user_existence_errors        = "ENABLED"
+  access_token_validity                = 15
+  refresh_token_validity               = 30
+  allowed_oauth_flows                  = ["code"]
+  callback_urls                        = ["https://example.com/"]
+  logout_urls                          = ["https://example.com/"]
+
+  allowed_oauth_scopes = [
+    "aws.cognito.signin.user.admin",
+    "email",
+    "openid",
+    "phone",
+    "profile",
+  ]
+
+  token_validity_units {
+    access_token  = "minutes"
+    refresh_token = "days"
+  }
+
+  supported_identity_providers = [
+    "COGNITO",
+  ]
+
+  explicit_auth_flows = [
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+  ]
+}
+
+resource "aws_cognito_user_pool_domain" "domain" {
+  domain       = "${var.app_name}-${var.env}"
+  user_pool_id = aws_cognito_user_pool.pool.id
+}
