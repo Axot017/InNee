c5d604f769b6c157d6b773a138053cabb196531d
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2023-01-03 21:51:44 +0100

    feat: add `create_profile_v1` lambda to infrastructure

--- infra/module/in-nee-module/main.tf
+++ infra/module/in-nee-module/main.tf
@@ -33,6 +33,21 @@ module "get_profile_v1_lambda" {
   ]
 }
 
+module "create_profile_v1_lambda" {
+  source = "../lambda-module"
+
+  env                   = var.env
+  name                  = "create-profile-v1"
+  app_name              = local.app_name
+  memory_size           = 128
+  zip_path              = "${path.module}/../../../target/lambdas/create_profile_v1.zip"
+  gateway_execution_arn = module.gateway.execution_arn
+  policies = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  ]
+}
+
 module "create_apartment_v1_lambda" {
   source = "../lambda-module"
 
@@ -67,7 +82,13 @@ module "gateway" {
       route             = "/v1/profile/current"
       method            = "GET"
       protected         = true
-    }
+    },
+    {
+      lambda_invoke_arn = module.create_profile_v1_lambda.invoke_arn
+      route             = "/v1/profile/current"
+      method            = "POST"
+      protected         = true
+    },
   ]
 }
 
